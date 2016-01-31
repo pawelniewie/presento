0c2186acc592ca0e2df86293d212cba85b4a840b
werckerbot <pleasemailus@wercker.com>
2016-01-31 21:24:31 +0000

    Deploy from pawelniewiadomski

--- src/components/renderers/renderers.js
+++
@@ -1,9 +0,0 @@
-import SingleSlide from './SingleSlide'
-import Booklet from './Booklet'
-import Preview from './Preview'
-
-export default {
-  single: SingleSlide,
-  booklet: Booklet,
-  preview: Preview
-}
--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { routeReducer as router } from 'react-router-redux'
-import presentation from './modules/presentation'
-
-export default combineReducers({
-  presentation,
-  router
-})
